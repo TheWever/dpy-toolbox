9316d98adc99b90d4852de78f97b3f5e6500e1b8
TheWever <85771814+TheWever@users.noreply.github.com>
2022-10-27 22:51:42 +0200

    Adding examples for new cmds

---
+++ examples/CachedTextChannel.py
@@ -0,0 +1,20 @@
+from discord.ext import commands
+from dpy_toolbox import Bot
+import discord
+
+bot = Bot(command_prefix='!', intents=discord.Intents.all())
+TOKEN = ''  # BAD
+
+@bot.command()
+async def hello(ctx: commands.Context):
+    # dont log like this use on_message
+    # this will not send a new request if the channel has been fetched previously
+    # => prevents rate limits due to fetching
+    log_channel = await bot.get_channel_from_cache("880934899622740072")
+    await log_channel.send(f"{ctx.author} ({ctx.author.id}) used hello in {ctx.channel.mention}")
+
+@bot.event
+async def on_ready():
+    print(f'Running as {bot.user}')
+
+bot.run(TOKEN)
