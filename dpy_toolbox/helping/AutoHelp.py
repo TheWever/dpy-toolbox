5e12aadf1f75f5fc5228ce8763e1f0b82f2bc43c
TheWever <85771814+TheWever@users.noreply.github.com>
2022-11-01 15:27:06 +0100

    Added send_command_help
    also updated the AutoHelp class

--- dpy_toolbox/helping/AutoHelp.py
+++ dpy_toolbox/helping/AutoHelp.py
@@ -1,5 +1,5 @@
 from discord.ext.commands.bot import BotBase
-from typing import Any, Iterable, Optional
+from typing import Any, Iterable, Optional, Union, Tuple
 from discord.ext import commands
 import discord
 
@@ -62,6 +62,9 @@ class AutoHelp(commands.MinimalHelpCommand):
         self._command_impl = command
         self.bot = bot
 
+    async def send_command_help(self, command: commands.Command[Any, ..., Any], /) -> None:
+        await self.get_destination().send(**self.sendable((command, )))
+
     def make_embed(self, help_for: list[Iterable[str]]):
         embed = discord.Embed(
             title=self.title,
@@ -74,7 +77,7 @@ class AutoHelp(commands.MinimalHelpCommand):
         for field in help_for:
             field_name, field_value = field[0], field[1]
             embed.add_field(
-                name=field_name,
+                name=field_name, #(f"{desc}\n{'=' * int(len(desc) * 0.81)}\n" if desc else command.short_doc or '')
                 value=field_value
             )
         return embed
@@ -82,34 +85,35 @@ class AutoHelp(commands.MinimalHelpCommand):
     def make_message(self, help_for: list[Iterable[str]]):
         return f"**{self.title}**" + "\n" + "\n".join([f"> {x[0]}\n```{x[1]}```" for x in help_for])
 
-    def format_help(self) -> list[Iterable[str]]:
-        help_for: list[Iterable[str]] = []
-        for command in self.bot.commands:
-            if not (h := getattr(command, 'helping', None)):
-                continue
+    def format_command(self, command: commands.Command) -> Optional[tuple[str, str]]:
+        if not (h := getattr(command, 'helping', None)):
+            return None
 
-            desc = h['description']
-            help_for.append((
-                f"{command.name}: {', '.join(h['params_name'].get(x, x) for x in command.params)}",  # field name
-                (f"{desc}\n{'=' * int(len(desc) * 0.81) }\n" if desc else command.short_doc or '') +  # field value
-                f"\n".join(
-                    [f"{h['params_name'].get(k, k)} {self.translator.get(v, None) or self.translator.get('not_found', 'any')} "
-                     f"{self.translator.get('required', 'required') if v.required else self.translator.get('not_required', 'not required')}"
-                     f"\n{h['params_desc'].get(k, '')}" for k, v in command.params.items()])
-            ))
-        return help_for
+        return (
+            f"{command.name}: {', '.join(h['params_name'].get(x, x) for x in command.params)}",  # field name
+            f"\n".join(
+                [
+                    f"{h['params_name'].get(k, k)} {self.translator.get(v, None) or self.translator.get('not_found', 'any')} "
+                    f"{self.translator.get('required', 'required') if v.required else self.translator.get('not_required', 'not required')}"
+                    f"\n{h['params_desc'].get(k, '')}" for k, v in command.params.items()])
+        )
 
-    @property
-    def sendable(self):
+    def format_help(self, cmds: Iterable[commands.Command]) -> tuple[tuple[str, str], ...]:
+        return tuple(filter(None, [self.format_command(cmd) for cmd in cmds]))
+
+    def sendable(
+            self,
+            cmds: Union[None, set[commands.Command[None, Any, Any]], Iterable[commands.Command]] = None
+    ) -> dict[str, Union[str, discord.Embed]]:
         return {
-            "content": self.make_message(self.format_help())
+            "content": self.make_message(self.format_help(cmds or self.bot.commands))
         } if self.message_type == 1 else {
-            "embed": self.make_embed(self.format_help())
+            "embed": self.make_embed(self.format_help(cmds or self.bot.commands))
         }
 
     async def send_pages(self):
         destination = self.get_destination()
-        await destination.send(**self.sendable)
+        await destination.send(**self.sendable(self.bot.commands))
 
 def helpful(*description: str) -> Any:
     def decorator(command: commands.Command):
